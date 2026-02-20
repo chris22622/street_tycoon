d9f6741ba8f036cc4b01cc34528f60c125f88f6d
ChrisLeslieDev <chrissanoleslie1990@gmail.com>
2026-02-20 00:40:54 -0500

    Introduce MapExtension and refactor PrestigeSystem
    Added MapExtension for casting Maps and refactored PrestigeSystem to PrestigeMetaSystem.

--- lib/data/social_endgame_models.dart
+++ lib/data/social_endgame_models.dart
@@ -1,6 +1,10 @@
 // Social Impact & Endgame Models
 import 'package:flutter/foundation.dart';
 
+extension MapExtension<K, V> on Map<K, V> {
+  Map<K, V> cast<NK, NV>() => Map<NK, NV>.from(this.cast<NK, NV>()) as Map<K, V>;
+}
+
 @immutable
 class SocialImpact {
   final String communityId;
@@ -46,6 +50,34 @@ class SocialImpact {
       communityTrust: communityTrust ?? this.communityTrust,
       economicImpact: economicImpact ?? this.economicImpact,
       lastAssessment: lastAssessment ?? this.lastAssessment,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'communityId': communityId,
+      'communityName': communityName,
+      'consequences': consequences,
+      'affectedPopulation': affectedPopulation,
+      'positivePrograms': positivePrograms,
+      'negativeEffects': negativeEffects,
+      'communityTrust': communityTrust,
+      'economicImpact': economicImpact,
+      'lastAssessment': lastAssessment.toIso8601String(),
+    };
+  }
+
+  factory SocialImpact.fromJson(Map<String, dynamic> json) {
+    return SocialImpact(
+      communityId: json['communityId'],
+      communityName: json['communityName'],
+      consequences: Map<String, double>.from(json['consequences']),
+      affectedPopulation: Map<String, int>.from(json['affectedPopulation']),
+      positivePrograms: List<String>.from(json['positivePrograms']),
+      negativeEffects: List<String>.from(json['negativeEffects']),
+      communityTrust: json['communityTrust'],
+      economicImpact: Map<String, double>.from(json['economicImpact']),
+      lastAssessment: DateTime.parse(json['lastAssessment']),
     );
   }
 }
@@ -103,6 +135,36 @@ class RehabilitationProgram {
   }
 
   double get utilizationRate => currentParticipants / capacity;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'type': type,
+      'capacity': capacity,
+      'currentParticipants': currentParticipants,
+      'successRate': successRate,
+      'monthlyCost': monthlyCost,
+      'reputationBonus': reputationBonus,
+      'requirements': requirements,
+      'outcomes': outcomes,
+    };
+  }
+
+  factory RehabilitationProgram.fromJson(Map<String, dynamic> json) {
+    return RehabilitationProgram(
+      id: json['id'],
+      name: json['name'],
+      type: json['type'],
+      capacity: json['capacity'],
+      currentParticipants: json['currentParticipants'],
+      successRate: json['successRate'],
+      monthlyCost: json['monthlyCost'],
+      reputationBonus: json['reputationBonus'],
+      requirements: List<String>.from(json['requirements']),
+      outcomes: Map<String, int>.from(json['outcomes']),
+    );
+  }
 }
 
 @immutable
@@ -146,6 +208,32 @@ class EnvironmentalImpact {
       healthEffects: healthEffects ?? this.healthEffects,
       mitigationMeasures: mitigationMeasures ?? this.mitigationMeasures,
       environmentalScore: environmentalScore ?? this.environmentalScore,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'operationId': operationId,
+      'operationType': operationType,
+      'pollutionLevels': pollutionLevels,
+      'affectedAreas': affectedAreas,
+      'cleanupCost': cleanupCost,
+      'healthEffects': healthEffects,
+      'mitigationMeasures': mitigationMeasures,
+      'environmentalScore': environmentalScore,
+    };
+  }
+
+  factory EnvironmentalImpact.fromJson(Map<String, dynamic> json) {
+    return EnvironmentalImpact(
+      operationId: json['operationId'],
+      operationType: json['operationType'],
+      pollutionLevels: Map<String, double>.from(json['pollutionLevels']),
+      affectedAreas: List<String>.from(json['affectedAreas']),
+      cleanupCost: json['cleanupCost'],
+      healthEffects: Map<String, int>.from(json['healthEffects']),
+      mitigationMeasures: List<String>.from(json['mitigationMeasures']),
+      environmentalScore: json['environmentalScore'],
     );
   }
 }
@@ -197,6 +285,34 @@ class CharitySystem {
       isPublic: isPublic ?? this.isPublic,
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'charityId': charityId,
+      'name': name,
+      'cause': cause,
+      'totalDonated': totalDonated,
+      'monthlyContribution': monthlyContribution,
+      'publicVisibility': publicVisibility,
+      'reputationGain': reputationGain,
+      'impact': impact,
+      'isPublic': isPublic,
+    };
+  }
+
+  factory CharitySystem.fromJson(Map<String, dynamic> json) {
+    return CharitySystem(
+      charityId: json['charityId'],
+      name: json['name'],
+      cause: json['cause'],
+      totalDonated: json['totalDonated'],
+      monthlyContribution: json['monthlyContribution'],
+      publicVisibility: json['publicVisibility'],
+      reputationGain: json['reputationGain'],
+      impact: Map<String, int>.from(json['impact']),
+      isPublic: json['isPublic'] ?? false,
+    );
+  }
 }
 
 @immutable
@@ -244,6 +360,34 @@ class EndgameScenario {
       difficulty: difficulty ?? this.difficulty,
       legacyEffects: legacyEffects ?? this.legacyEffects,
       isUnlocked: isUnlocked ?? this.isUnlocked,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'type': type,
+      'requirements': requirements,
+      'availableChoices': availableChoices,
+      'outcomes': outcomes,
+      'difficulty': difficulty,
+      'legacyEffects': legacyEffects,
+      'isUnlocked': isUnlocked,
+    };
+  }
+
+  factory EndgameScenario.fromJson(Map<String, dynamic> json) {
+    return EndgameScenario(
+      id: json['id'],
+      name: json['name'],
+      type: json['type'],
+      requirements: Map<String, int>.from(json['requirements']),
+      availableChoices: List<String>.from(json['availableChoices']),
+      outcomes: Map<String, String>.from(json['outcomes']),
+      difficulty: json['difficulty'],
+      legacyEffects: Map<String, double>.from(json['legacyEffects']),
+      isUnlocked: json['isUnlocked'] ?? false,
     );
   }
 }
@@ -295,6 +439,34 @@ class LegacySystem {
       challenges: challenges ?? this.challenges,
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'heirId': heirId,
+      'name': name,
+      'inheritedAssets': inheritedAssets,
+      'inheritedSkills': inheritedSkills,
+      'inheritedReputation': inheritedReputation,
+      'inheritedRelationships': inheritedRelationships,
+      'startingAdvantage': startingAdvantage,
+      'specialAbilities': specialAbilities,
+      'challenges': challenges,
+    };
+  }
+
+  factory LegacySystem.fromJson(Map<String, dynamic> json) {
+    return LegacySystem(
+      heirId: json['heirId'],
+      name: json['name'],
+      inheritedAssets: Map<String, int>.from(json['inheritedAssets']),
+      inheritedSkills: Map<String, double>.from(json['inheritedSkills']),
+      inheritedReputation: List<String>.from(json['inheritedReputation']),
+      inheritedRelationships: Map<String, String>.from(json['inheritedRelationships']),
+      startingAdvantage: json['startingAdvantage'],
+      specialAbilities: List<String>.from(json['specialAbilities']),
+      challenges: Map<String, int>.from(json['challenges']),
+    );
+  }
 }
 
 @immutable
@@ -336,10 +508,34 @@ class NewGamePlus {
       exclusiveContent: exclusiveContent ?? this.exclusiveContent,
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'playthrough': playthrough,
+      'unlockedFeatures': unlockedFeatures,
+      'bonuses': bonuses,
+      'carriedAchievements': carriedAchievements,
+      'permanentUpgrades': permanentUpgrades,
+      'difficultyMultiplier': difficultyMultiplier,
+      'exclusiveContent': exclusiveContent,
+    };
+  }
+
+  factory NewGamePlus.fromJson(Map<String, dynamic> json) {
+    return NewGamePlus(
+      playthrough: json['playthrough'],
+      unlockedFeatures: Map<String, bool>.from(json['unlockedFeatures']),
+      bonuses: Map<String, double>.from(json['bonuses']),
+      carriedAchievements: List<String>.from(json['carriedAchievements']),
+      permanentUpgrades: Map<String, int>.from(json['permanentUpgrades']),
+      difficultyMultiplier: json['difficultyMultiplier'],
+      exclusiveContent: List<String>.from(json['exclusiveContent']),
+    );
+  }
 }
 
 @immutable
-class PrestigeSystem {
+class PrestigeMetaSystem {
   final int prestigeLevel;
   final int prestigePoints;
   final Map<String, bool> prestigeUpgrades;
@@ -348,7 +544,7 @@ class PrestigeSystem {
   final DateTime lastPrestige;
   final int totalPrestiges;
 
-  const PrestigeSystem({
+  const PrestigeMetaSystem({
     required this.prestigeLevel,
     required this.prestigePoints,
     required this.prestigeUpgrades,
@@ -358,7 +554,7 @@ class PrestigeSystem {
     required this.totalPrestiges,
   });
 
-  PrestigeSystem copyWith({
+  PrestigeMetaSystem copyWith({
     int? prestigeLevel,
     int? prestigePoints,
     Map<String, bool>? prestigeUpgrades,
@@ -367,7 +563,7 @@ class PrestigeSystem {
     DateTime? lastPrestige,
     int? totalPrestiges,
   }) {
-    return PrestigeSystem(
+    return PrestigeMetaSystem(
       prestigeLevel: prestigeLevel ?? this.prestigeLevel,
       prestigePoints: prestigePoints ?? this.prestigePoints,
       prestigeUpgrades: prestigeUpgrades ?? this.prestigeUpgrades,
@@ -375,6 +571,30 @@ class PrestigeSystem {
       globalBonuses: globalBonuses ?? this.globalBonuses,
       lastPrestige: lastPrestige ?? this.lastPrestige,
       totalPrestiges: totalPrestiges ?? this.totalPrestiges,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'prestigeLevel': prestigeLevel,
+      'prestigePoints': prestigePoints,
+      'prestigeUpgrades': prestigeUpgrades,
+      'prestigeRewards': prestigeRewards,
+      'globalBonuses': globalBonuses,
+      'lastPrestige': lastPrestige.toIso8601String(),
+      'totalPrestiges': totalPrestiges,
+    };
+  }
+
+  factory PrestigeMetaSystem.fromJson(Map<String, dynamic> json) {
+    return PrestigeMetaSystem(
+      prestigeLevel: json['prestigeLevel'],
+      prestigePoints: json['prestigePoints'],
+      prestigeUpgrades: Map<String, bool>.from(json['prestigeUpgrades']),
+      prestigeRewards: List<String>.from(json['prestigeRewards']),
+      globalBonuses: Map<String, double>.from(json['globalBonuses']),
+      lastPrestige: DateTime.parse(json['lastPrestige']),
+      totalPrestiges: json['totalPrestiges'],
     );
   }
 }
